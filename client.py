76c7742441f1e48708a894b93cf7f0079c56e54e
Jaine Rannow Budke <jainebudke@A304-20L.imd.local>
2018-10-26 12:16:11 -0300

    conexao

--- client.py
+++ client.py
@@ -1,12 +1,62 @@
-import socket
-HOST = '10.7.119.4'    # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print 'Para sair use CTRL+X\n'
-msg = raw_input()
-while msg <> '\x18':
-    tcp.send (msg)
-    msg = raw_input()
-tcp.close()
+from socket import socket,AF_INET,SOCK_STREAM
+from threading import Thread
+ 
+#classe para manipular o socket
+class Send:
+    def __init__(self):
+        self.__msg=''
+        self.new=True
+        self.con=None
+    
+    def put(self,msg):
+        self.__msg=msg
+        if self.con != None:
+            #envia um mensagem atravez de uma conexão socket
+            self.con.send(str.encode(self.__msg))
+ 
+    def get(self):
+        return self.__msg
+ 
+    def loop(self):
+        return self.new
+ 
+#função esperar - Thread
+def esperar(tcp,send,host='localhost',port=5000):
+    destino=(host,port)
+    #conecta a um servidor
+    tcp.connect(destino)
+    
+    while send.loop():
+        print('Conectado a ',host,'.')
+        #atribui a conexão ao manipulador
+        send.con=tcp
+
+        while send.loop():
+            #aceita uma mensagem
+            msg=tcp.recv(1024)
+            if not msg: break
+            print(str(msg,'utf-8'))
+ 
+if __name__ == '__main__':
+    print('Digite o nome ou IP do servidor(localhost): ')
+    host=input()
+  
+    if host=='':
+        host = '127.0.0.1'
+  
+    #cria um socket
+    tcp=socket(AF_INET,SOCK_STREAM)
+    send=Send()
+    #cria um Thread e usa a função esperar com dois argumentos
+    processo=Thread(target=esperar,args=(tcp,send,host))
+    processo.start()
+    print('')
+  
+    msg=input()
+    while True:
+        send.put(msg)
+        msg=input()
+    
+    processo.join()
+    tcp.close()
+    exit()
--- server.py
+++ server.py
@@ -1,16 +1,62 @@
-import socket
-HOST = ''              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen(1)
-while True:
-    con, cliente = tcp.accept()
-    print 'Concetado por', cliente
+from socket import socket,AF_INET,SOCK_STREAM
+from threading import Thread
+ 
+#classe para manipular o socket
+class Send:
+    def __init__(self):
+        self.__msg=''
+        self.new=True
+        self.con=None
+        
+    def put(self,msg):
+        self.__msg=msg
+        if self.con != None:
+            #envia um mensagem atravez de uma conexão socket
+            self.con.send(str.encode(self.__msg))
+    
+    def get(self):
+        return self.__msg
+    
+    def loop(self):
+        return self.new
+ 
+#função esperar - Thread
+def esperar(tcp,send,host='',port=5000):
+    origem=(host,port)
+    #cria um vinculo
+    tcp.bind(origem)
+    #deixa em espera
+    tcp.listen(1)
+    
     while True:
-        msg = con.recv(1024)
+        #aceita um conexão
+        con,cliente=tcp.accept()
+        print('Cliente ',cliente,' conectado!')
+        #atribui a conexão ao manipulador
+        send.con=con
+        
+    while True:
+        #aceita uma mensagem
+        msg=con.recv(1024)
         if not msg: break
-        print cliente, msg
-    print 'Finalizando conexao do cliente', cliente
-    con.close()
+        print(str(msg,'utf-8'))
+
+if __name__ == '__main__':
+    #cria um socket
+    tcp=socket(AF_INET,SOCK_STREAM)
+    send=Send()
+    #cria um Thread e usa a função esperar com dois argumentos
+    processo=Thread(target=esperar,args=(tcp,send))
+    processo.start()
+
+print('Iniciando o servidor de chat!')
+print('Aguarde alguém conectar!')
+
+msg=input()
+while True:
+    send.put(msg)
+    msg=input()
+
+processo.join()
+tcp.close()
+exit()
