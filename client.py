a47ef4c90aef06cfa0f0ca7c54151eb38ee21c42
Mayra Azevedo <mayradazevedo@gmail.com>
2018-11-16 21:12:00 -0300

    fix #5, send key pressed to server

--- client.py
+++ client.py
@@ -2,39 +2,71 @@
 import socket 
 import select 
 import sys 
+import curses
+from thread import *
+from threading import Thread
+from time import sleep
 
+def screenthread(read_sockets,server,win):
+    '''Thread that updates the window'''
+    while True:
+        for socks in read_sockets: 
+            if socks == server: 
+                message = socks.recv(2048) 
+                data = str(message).split(',')
+                print(data)
+                #win.addch(int(data[0]), int(data[1]), data[2])
+
+def sendthread(win,server, key):
+    '''Thread that send te key stroke sginal to the server'''
+    while True:
+        prevKey = key
+        event = win.getch()
+        if( event != -1):
+            key =  event
+            # if key pressed changes
+            if( key != prevKey):
+                server.send(str(key))
+                prevKey = key
+
+#################################
+## Begin client code
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 if len(sys.argv) != 3: 
-	print "Correct usage: script, IP address, port number"
-	exit() 
+    print "Correct usage: script, IP address, port number"
+    exit() 
 IP_address = str(sys.argv[1]) 
 Port = int(sys.argv[2]) 
 server.connect((IP_address, Port)) 
 
-while True: 
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)  
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)
+#win = None
+key = curses.KEY_RIGHT
 
-	# maintains a list of possible input streams 
-	sockets_list = [sys.stdin, server] 
+while key != 27:
+    #win.border(0)
+    #win.addstr(0, 2, 'Score :+ ')                # Printing 'Score' and
+    #win.addstr(0, 27, ' SNAKE ')
+    # maintains a list of possible input streams 
+    sockets_list = [sys.stdin, server] 
 
-	""" There are two possible input situations. Either the 
-	user wants to give manual input to send to other people, 
-	or the server is sending a message to be printed on the 
-	screen. Select returns from sockets_list, the stream that 
-	is reader for input. So for example, if the server wants 
-	to send a message, then the if condition will hold true 
-	below.If the user wants to send a message, the else 
-	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+    """ There are two possible input situations. Either the 
+    user wants to give manual input to send to other people, 
+    or the server is sending a message to be printed on the 
+    screen. Select returns from sockets_list, the stream that 
+    is reader for input. So for example, if the server wants 
+    to send a message, then the if condition will hold true 
+    below.If the user wants to send a message, the else 
+    condition will evaluate as true"""
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+    #start_new_thread(screenthread,(read_sockets,server,win))
+    start_new_thread(sendthread,(win,server, key))
 
-	for socks in read_sockets: 
-		if socks == server: 
-			message = socks.recv(2048) 
-			print message 
-		else: 
-			message = sys.stdin.readline() 
-			server.send(message) 
-			sys.stdout.write("<You>") 
-			sys.stdout.write(message) 
-			sys.stdout.flush() 
+curses.endwin()
 server.close() 
-
--- server.py
+++ server.py
@@ -16,8 +16,8 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # checks whether sufficient arguments have been provided 
 if len(sys.argv) != 3: 
-	print "Correct usage: script, IP address, port number"
-	exit() 
+    print "Correct usage: script, IP address, port number"
+    exit() 
 
 # takes the first argument from command prompt as IP address 
 IP_address = str(sys.argv[1]) 
@@ -42,79 +42,77 @@ list_of_clients = []
 
 def clientthread(conn, addr): 
 
-	# sends a message to the client whose user object is conn 
-	conn.send("Welcome to this chatroom!") 
+    while True: 
+            try: 
+                message = conn.recv(2048) 
+                if message: 
 
-	while True: 
-			try: 
-				message = conn.recv(2048) 
-				if message: 
+                    """prints the message and address of the 
+                    user who just sent the message on the server 
+                    terminal"""
+                    key = int(message)
+                    print(key)
 
-					"""prints the message and address of the 
-					user who just sent the message on the server 
-					terminal"""
-					print "<" + addr[0] + "> " + message 
+                    # Calls broadcast function to send message to all 
+                    #message_to_send = "<" + addr[0] + "> " + message 
+                    #broadcast(message_to_send, conn) 
 
-					# Calls broadcast function to send message to all 
-					message_to_send = "<" + addr[0] + "> " + message 
-					broadcast(message_to_send, conn) 
+                else: 
+                    """message may have no content if the connection 
+                    is broken, in this case we remove the connection"""
+                    remove(conn) 
 
-				else: 
-					"""message may have no content if the connection 
-					is broken, in this case we remove the connection"""
-					remove(conn) 
-
-			except: 
-				continue
+            except: 
+                continue
 
 """Using the below function, we broadcast the message to all 
 clients who's object is not the same as the one sending 
 the message """
 def broadcast(message, connection): 
-	for clients in list_of_clients: 
-		if clients!=connection: 
-			try: 
-				clients.send(message) 
-			except: 
-				clients.close() 
+    for clients in list_of_clients: 
+        if clients!=connection: 
+            try: 
+                clients.send(message) 
+            except: 
+                clients.close() 
 
-				# if the link is broken, we remove the client 
-				remove(clients) 
+                # if the link is broken, we remove the client 
+                remove(clients) 
 
 """The following function simply removes the object 
 from the list that was created at the beginning of 
 the program"""
 def remove(connection): 
-	if connection in list_of_clients: 
-		list_of_clients.remove(connection) 
+    if connection in list_of_clients: 
+        list_of_clients.remove(connection) 
 
 while True: 
 
-	# get instance of game
-	game = snake.game()
-	game.execute()
+    # get instance of game
+    game = snake.game()
+    game.execute()
 
 
-	"""Accepts a connection request and stores two parameters, 
-	conn which is a socket object for that user, and addr 
-	which contains the IP address of the client that just 
-	connected"""
-	conn, addr = server.accept() 
+    """Accepts a connection request and stores two parameters, 
+    conn which is a socket object for that user, and addr 
+    which contains the IP address of the client that just 
+    connected"""
+    conn, addr = server.accept() 
 
-	"""Maintains a list of clients for ease of broadcasting 
-	a message to all available people in the chatroom"""
-	list_of_clients.append(conn) 
+    """Maintains a list of clients for ease of broadcasting 
+    a message to all available people in the chatroom"""
+    list_of_clients.append(conn) 
 
-	# prints the address of the user that just connected 
-	print addr[0] + " connected"
+    # prints the address of the user that just connected 
+    # print addr[0] + " connected"
 
-	# quando aceitar conexao, cria cobra
-	#game.createSnake(1)
+    # quando aceitar conexao, cria cobra
+    game.createSnake(1)
 
 
-	# creates and individual thread for every user 
-	# that connects 
-	start_new_thread(clientthread,(conn,addr))	 
+    # creates and individual thread for every user 
+    # that connects 
+    start_new_thread(clientthread,(conn,addr))     
 
-conn.close() 
+#conn.close() 
 server.close() 
