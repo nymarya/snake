86e40748f5429e583513c25080bab9e04ade1e46
jainebudke <jainebudke@hotmail.com>
2018-11-22 20:03:53 -0300

    kill snake

--- snake.py
+++ snake.py
@@ -69,73 +69,79 @@ class game:
                 
                 for id, value in self.clients.items():
                 
-                    #recupera um cliente 
-                    client = self.clients[id]   
-
-                    # recupera o snake do cliente atual
-                    snake = client[0]
-                    # recupera a tecla clicada pelo cliente 
-                    key = client[1]       
-                    # recupera a tecla da iteracao passada
-                    prevKey = client[2]
-                                
-                    # Increases the speed of Snake as its length increases
-                    win.timeout(300)          
-                    
-                    time.sleep(1)
-                    
-                    #prevKey = key                                                 
-                    #event = win.getch()
-                    #event = key
-                    #key = key if event == -1 else event 
-                    
-                    '''
-                    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
-                        key = prevKey
-
-                    
-                    # If snake runs over itself
-                    #if snake[0] in snake[1:]: break
-                    if abs(key-prevKey) != 1 or not(min(key, prevKey) != KEY_UP and max(key, prevKey) != KEY_LEFT):
-                        # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
-                        # This is taken care of later at [1].
-                        snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
-
-                    else:
-                        '''
-                    #key = prevKey
-                    snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
-
-
-                    # Exit if snake crosses the boundaries (Uncomment to enable)
-                    if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
-
-
-                    
-                    if snake[0] == food:                                            # When snake eats the food
-                        food = []
-                        score += 1
-                        while food == []:
-                            food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
-                            if food in snake: food = []
-                        win.addch(food[0], food[1], '*')
-                        broadcast(food[0], food[1], '*')
-                    else:    
-                        last = snake.pop()                                          # [1] If it does not eat the food, length decreases
-                        win.addch(last[0], last[1], ' ')
-                        broadcast(last[0], last[1], ' ')
-                      
-                            
-                    
                     try:
-                        win.addch(snake[0][0], snake[0][1], '#')
-                        broadcast(snake[0][0], snake[0][1], '#')
+                        #recupera um cliente 
+                        client = self.clients[id]   
+
+                        # recupera o snake do cliente atual
+                        snake = client[0]
+                        # recupera a tecla clicada pelo cliente 
+                        key = client[1]       
+                        # recupera a tecla da iteracao passada
+                        prevKey = client[2]
+                                    
+                        # Increases the speed of Snake as its length increases
+                        win.timeout(300)          
+                        
+                        #time.sleep(1)
+                        
+                        #prevKey = key                                                 
+                        event = win.getch()
+                        #event = key
+                        #key = key if event == -1 else event 
+                        
+                        '''
+                        if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+                            key = prevKey
+                        '''
+                        
+                        # If snake runs over itself
+                        #if snake[0] in snake[1:]: break
+                        if abs(key-prevKey) != 1 or not(min(key, prevKey) != KEY_UP and max(key, prevKey) != KEY_LEFT):
+                            # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
+                            # This is taken care of later at [1].
+                            snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+                            
+                        else:    
+                            #key = prevKey
+                            snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+
+
+                        # Exit if snake crosses the boundaries (Uncomment to enable)
+                        if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: 
+                            self.killSnake(id)
+                            break
+
+
+                        
+                        if snake[0] == food:                                            # When snake eats the food
+                            food = []
+                            score += 1
+                            while food == []:
+                                food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
+                                if food in snake: food = []
+                            win.addch(food[0], food[1], '*')
+                            broadcast(food[0], food[1], '*')
+                        else:    
+                            last = snake.pop()                                          # [1] If it does not eat the food, length decreases
+                            win.addch(last[0], last[1], ' ')
+                            broadcast(last[0], last[1], ' ')
+                        
+                                
+                        
+                        try:
+                            win.addch(snake[0][0], snake[0][1], '#')
+                            broadcast(snake[0][0], snake[0][1], '#')
+                        except:
+                            break
+
+                        # atualiza valores no map do cliente
+                        self.clients[id][0] = snake
+
                     except:
-                        break
+                        break 
                     
-                    
-                    # atualiza valores no map do cliente
-                    self.clients[id][0] = snake
+
             break          
 
     def execute(self, broadcast):
